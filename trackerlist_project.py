3ad64a8f5fcf6e778e2c88f25b070013c23539f1
abc1763613206 <abc1763613206@163.com>
2019-10-29 14:27:22 +0800

    move to jsdelivr

--- trackerlist_project.py
+++ trackerlist_project.py
@@ -8,7 +8,7 @@ import trackon
 def main():
     while True:
         tlist = requests.get(
-            "https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt"
+            "https://cdn.jsdelivr.net/gh/ngosang/trackerslist@master/trackers_all.txt"
         )
         trackon.enqueue_new_trackers(tlist.text)
         sleep(86400)
